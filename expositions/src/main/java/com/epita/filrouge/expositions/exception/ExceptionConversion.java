ec11c8e46ec56713eeede7994b384c1ec41386b5
mcdgit141 <mcdmail77@gmail.com>
2020-08-04 16:23:08 +0200

    Gestion des exceptions mais cela ne fonctionne pas, attention les tests sur la partie exposition ne passent pas à cause de la gestion des exceptions

--- expositions/src/main/java/com/epita/filrouge/expositions/exception/ExceptionConversion.java
+++ expositions/src/main/java/com/epita/filrouge/expositions/exception/ExceptionConversion.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.exc.ValueInstantiationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
