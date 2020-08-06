ddea748c1222e4889b901d06a16434fe38f6f467
damien vivier <vivdams@gmail.com>
2020-08-06 11:59:30 +0200

    Infrastructure - Affectation : ajout d'un test sur les valeurs des colonnes pour l'enregistrement inséré

--- infrastructure/src/test/java/com/epita/filrouge/infrastructure/affectation/RepositoryAffectationImplMockJpaTest.java
+++ infrastructure/src/test/java/com/epita/filrouge/infrastructure/affectation/RepositoryAffectationImplMockJpaTest.java
@@ -14,24 +14,28 @@ import com.epita.filrouge.infrastructure.iphone.ModeleIphoneEntity;
 import com.epita.filrouge.infrastructure.site.SiteExerciceEntity;
 import com.epita.filrouge.infrastructure.uo.UoEntity;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
-//@Disabled
-class RepositoryAffectationImplTest {
+class RepositoryAffectationImplMockJpaTest {
 
+    private static final Long SITE_ID = 1L;
     private static final String CODE_SITE = "V2";
     private static final String NOM_SITE = "Valmy2";
     private static final String ADRESSE_POSTALE = "41, Rue de Valmy";
@@ -40,19 +44,23 @@ class RepositoryAffectationImplTest {
     private static final String PAYS = "FRANCE";
     private static final LocalDate DATE_CREATION = LocalDate.now();
 
+    private static final Long UO_ID = 1L;
     private static final String CODE_UO = "SDI101";
     private static final String FONCTION_RATTACHEMENT = "BDDF IT";
     private static final String CODE_UO_PARENT = "SDI1";
     private static final String NOM_USAGE_UO = "DATAHUB";
     private static final String NOM_RESPONSABLE_UO = "Alfonse de la Renardiere";
 
+    private static final Long COLLABORATEUR_ID = 1L;
     private static final String COLLABORATEUR_UID = "666999";
     private static final String COLLABORATEUR_NOM = "Doe";
     private static final String COLLABORATEUR_PRENOM = "John";
     private static final String COLLABORATEUR_NUMEROLIGNE = "0612345678";
 
+    private static final Long MODELE_ID = 1L;
     private static final String MODELE_NOMMODELE = "Iphone8";
 
+    private static final Long IPHONE_ID = 1L;
     private static final String IPHONE_NUMEROSERIE = "123456";
     private static final Double IPHONE_PRIX = 800D;
     private static final EtatIphoneEnum IPHONE_ETAT = EtatIphoneEnum.DISPONIBLE;
@@ -68,6 +76,8 @@ class RepositoryAffectationImplTest {
     private IphoneEntity monIphoneEntityPersiste;
     private AffectationEntity monAffectationEntityPersiste;
 
+    @Autowired
+    RepositoryCollaborateurImpl repositoryCollaborateurImpl;
 
     @Autowired
     IRepositoryAffectation repositoryAffectation;
@@ -80,6 +90,9 @@ class RepositoryAffectationImplTest {
 
     @Autowired
     IphoneEntityMapper iphoneEntityMapper;
+
+    @MockBean
+    IRepositoryJpaAffectation repositoryJpaAffectation;
 
     @BeforeEach
     public void init() {
@@ -122,27 +135,11 @@ class RepositoryAffectationImplTest {
         iPhoneEntity.setEtatIphone(IPHONE_ETAT);
         monIphoneEntityPersiste = entityManager.persistAndFlush(iPhoneEntity);
 
+//        System.out.println("collaborateurEntity.getCollaborateurId() = " + collaborateurEntity.getCollaborateurId());
+//        System.out.println("iPhoneEntity.getIphoneId() = " + iPhoneEntity.getIphoneId());
+//        System.out.println("modeleIphoneEntity.getModeleId() = " + modeleIphoneEntity.getModeleId());
     }
 
-    @Test
-    @DisplayName("Doit créer un enregistrement Affectation")
-    void ShouldCreateAnAffectation() {
-//        //Given
-        Collaborateur collaborateur = collaborateurEntityMapper.mapToDomain(monCollaborateurEntityPersiste);
-
-        Iphone iphone = iphoneEntityMapper.mapToDomain(monIphoneEntityPersiste);
-
-        Affectation affectationACreer = new Affectation(AFFECTATION_NUMERO, AFFECTATION_DATE, AFFECTATION_COMMENTAIRE,collaborateur, iphone);
-
-////        //When
-        repositoryAffectation.affecter(affectationACreer);
-////        //Then
-        final List<?> affectationRecues = entityManager.getEntityManager()
-                .createQuery("select o from AffectationEntity o where o.numeroAffectation = :numeroAffectation")
-                .setParameter("numeroAffectation", AFFECTATION_NUMERO)
-                .getResultList();
-        assertThat(affectationRecues.size()).isEqualTo(1);
-    }
 
     @Test
     @DisplayName("Doit créer un enregistrement Affectation avec les bonnes valeurs")
@@ -150,60 +147,30 @@ class RepositoryAffectationImplTest {
 //        //Given
         Collaborateur collaborateur = collaborateurEntityMapper.mapToDomain(monCollaborateurEntityPersiste);
 
+
         Iphone iphone = iphoneEntityMapper.mapToDomain(monIphoneEntityPersiste);
 
         Affectation affectationACreer = new Affectation(AFFECTATION_NUMERO, AFFECTATION_DATE, AFFECTATION_COMMENTAIRE,collaborateur, iphone);
 
+        ArgumentCaptor<AffectationEntity> affectationEntityCaptor = ArgumentCaptor.forClass(AffectationEntity.class);
+        // AffectationEntity : c'est la classe qui est envoyée à  repositoryJpaAffectation par repositoryAffectation.affecter
+        // On va capturer cette classe, car on veut voir ce que repositoryAffectation.affecter va  transmettre
+        // à repositoryJpaAffectation
+
+
 ////        //When
         repositoryAffectation.affecter(affectationACreer);
 ////        //Then
-        final List<?> affectationRecues = entityManager.getEntityManager()
-                .createQuery("select o from AffectationEntity o where o.numeroAffectation = :numeroAffectation")
-                .setParameter("numeroAffectation", AFFECTATION_NUMERO)
-                .getResultList();
-        AffectationEntity affectationEntityGenerated = (AffectationEntity) affectationRecues.get(0);
-        CollaborateurEntity collaborateurEntityGenerated = affectationEntityGenerated.getCollaborateur();
-//        assertThat(affectationEntityGenerated.getCollaborateur().getNom()).isEqualTo(COLLABORATEUR_NOM);
+        verify(repositoryJpaAffectation).save(affectationEntityCaptor.capture());
+
+        AffectationEntity affectationEntityGenerated =affectationEntityCaptor.getValue();
+        // On recupere la classe AffectationEntity capturée
+
         assertThat(affectationEntityGenerated)
-                .extracting(AffectationEntity::getNumeroAffectation,
-                            AffectationEntity::getDateAffectation,
-                            AffectationEntity::getCommentaire
-                            )
-                .containsExactly(AFFECTATION_NUMERO,
-                                 AFFECTATION_DATE,
-                                 AFFECTATION_COMMENTAIRE
-                );
-        assertThat(collaborateurEntityGenerated)
-                .extracting(CollaborateurEntity::getUid,
-                            CollaborateurEntity::getNom,
-                            CollaborateurEntity::getPrenom,
-                            CollaborateurEntity::getNumeroLigne
-                            )
-                .containsExactly(COLLABORATEUR_UID,
-                                 COLLABORATEUR_NOM,
-                                 COLLABORATEUR_PRENOM,
-                                 COLLABORATEUR_NUMEROLIGNE
-                );
+                .extracting(AffectationEntity::getDateAffectation,
+                            AffectationEntity::getCommentaire)
+                .containsExactly(AFFECTATION_DATE,AFFECTATION_COMMENTAIRE);
     }
 
-    @Test
-//    @Disabled
-    @DisplayName("Recherche Affectation par filtres")
-    public void Should_return_Affectations_giving_filters(){
-        //giving
-        AffectationEntity affectationEntity = new AffectationEntity();
-        affectationEntity.setCollaborateur(entityManager.find(CollaborateurEntity.class,entityManager.getId(monCollaborateurEntityPersiste)));
-        affectationEntity.setIphone(entityManager.find(IphoneEntity.class,entityManager.getId(monIphoneEntityPersiste)));
-        affectationEntity.setDateAffectation(LocalDate.now());
-        affectationEntity.setDateRenouvellementPrevue(LocalDate.now().plusYears(2));
-        monAffectationEntityPersiste = entityManager.persist(affectationEntity);
-        //when
 
-        List<Affectation> result = repositoryAffectation.rechercheAffectationAvecFiltres(monCollaborateurEntityPersiste.getUid(),monCollaborateurEntityPersiste.getNom(),monUoEntityPersiste.getCodeUo(),
-                monUoEntityPersiste.getNomUsageUo(),monSiteExercicePersiste.getNomSite(),monCollaborateurEntityPersiste.getNumeroLigne(),monModeleIphoneEntityPersiste.getNomModele(),
-                LocalDate.now(), null);
-
-        //then
-        assertThat(result.size()).isEqualTo(1);
-    }
 }
