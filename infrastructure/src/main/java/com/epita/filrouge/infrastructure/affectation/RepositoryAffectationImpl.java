a5ca9ec6e77dc2ea75bef71b509135746dac63f5
damien vivier <vivdams@gmail.com>
2020-08-07 14:56:31 +0200

    Infrastructure - Affectation : ajout de tests + refacto rechercheAffectationAvecFiltres suite préco Sonarlink

--- infrastructure/src/main/java/com/epita/filrouge/infrastructure/affectation/RepositoryAffectationImpl.java
+++ infrastructure/src/main/java/com/epita/filrouge/infrastructure/affectation/RepositoryAffectationImpl.java
@@ -2,14 +2,10 @@ package com.epita.filrouge.infrastructure.affectation;
 
 import com.epita.filrouge.domain.affectation.Affectation;
 import com.epita.filrouge.domain.affectation.IRepositoryAffectation;
-import com.epita.filrouge.domain.collaborateur.Collaborateur;
-import com.epita.filrouge.domain.iphone.Iphone;
-import com.epita.filrouge.domain.iphone.ModeleIphone;
 import com.epita.filrouge.infrastructure.collaborateur.CollaborateurEntity;
 import com.epita.filrouge.infrastructure.collaborateur.IRepositoryJpaCollaborateur;
 import com.epita.filrouge.infrastructure.iphone.IRepositoryJpaIphone;
 import com.epita.filrouge.infrastructure.iphone.IphoneEntity;
-import com.epita.filrouge.infrastructure.iphone.ModeleIphoneEntity;
 import com.epita.filrouge.infrastructure.uo.UoEntityMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -50,44 +46,10 @@ public class RepositoryAffectationImpl implements IRepositoryAffectation {
     @Override
     public void affecter(Affectation affectationACreer) {
 
-        System.out.println("affectationACreer = " + affectationACreer);
-//        Collaborateur collaborateur = affectationACreer.getCollaborateur();
-//        CollaborateurEntity collaborateurEntity = new CollaborateurEntity();
-////        collaborateurEntity.setCollaborateurId(collaborateur.getId());
-//        collaborateurEntity.setUid(collaborateur.getUid());
-//        collaborateurEntity.setNom(collaborateur.getNom());
-//        collaborateurEntity.setPrenom(collaborateur.getPrenom());
-//        collaborateurEntity.setNumeroLigne(collaborateur.getNumeroLigne());
-//
-//        collaborateurEntity.setUo(uoEntityMapper.mapToEntity(collaborateur.getUo()));
-//        collaborateurEntity.setAffectationCollaborateur(affectationEntityMapper.mapToEntityList(collaborateur.getAffectationCollaborateur()));
+        monLogger.debug("affectationACreer = {}" , affectationACreer);
+
 
         CollaborateurEntity monCollaborateurEntity = iRepositoryJpaCollaborateur.findByUid(affectationACreer.getCollaborateur().getUid());
-
-//        System.out.println("collaborateurEntity.getCollaborateurId() = " + collaborateurEntity.getCollaborateurId());
-//        System.out.println("collaborateurEntity.getUid() = " + collaborateurEntity.getUid());
-//        System.out.println("collaborateurEntity.getNom() = " + collaborateurEntity.getNom());
-//        System.out.println("collaborateurEntity.getPrenom() = " + collaborateurEntity.getPrenom());
-//        System.out.println("collaborateurEntity.getCollaborateurId() = " + collaborateurEntity.getCollaborateurId());
-
-//        Iphone iPhone = affectationACreer.getIphone();
-//        ModeleIphone modeleIphone = iPhone.getModeleIphone();
-
-//        ModeleIphoneEntity modeleIphoneEntity = new ModeleIphoneEntity();
-//        modeleIphoneEntity.setNomModele(modeleIphone.getNomModele());
-//        modeleIphoneEntity.setModeleId(modeleIphone.getModeleID());
-//        System.out.println("infra modeleIphoneEntity.getModeleId() = " + modeleIphoneEntity.getModeleId());
-//        System.out.println("infra modeleIphoneEntity.getNomModele() = " + modeleIphoneEntity.getNomModele());
-
-
-//        IphoneEntity iphoneEntity = new IphoneEntity();
-//        iphoneEntity.setIphoneId(iPhone.getIphoneId());
-//        iphoneEntity.setNumeroSerie(iPhone.getNumeroSerie());
-//        iphoneEntity.setEtatIphone(iPhone.getEtatIphone());
-//        iphoneEntity.setModeleIphoneEntity(modeleIphoneEntity);
-//        System.out.println("iphoneEntity.getIphoneId() = " + iphoneEntity.getIphoneId());
-//        System.out.println("iphoneEntity.getEtatIphone() = " + iphoneEntity.getEtatIphone());
-//        System.out.println("iphoneEntity.getNumeroSerie() = " + iphoneEntity.getNumeroSerie());
 
         IphoneEntity monIphoneEntity = iRepositoryJpaIphone.findByNumeroSerie(affectationACreer.getIphone().getNumeroSerie());
 
@@ -99,12 +61,12 @@ public class RepositoryAffectationImpl implements IRepositoryAffectation {
         affectationEntity.setDateFin(affectationACreer.getDateFin());
         affectationEntity.setCommentaire(affectationACreer.getCommentaire());
         affectationEntity.setMotifFin(affectationACreer.getMotifFin());
-//        affectationEntity.setCollaborateur(collaborateurEntity);
         affectationEntity.setCollaborateur(monCollaborateurEntity);
         affectationEntity.setIphone(monIphoneEntity);
 
         iRepositoryJpaAffectation.save(affectationEntity);
     }
+
 
     @Override
     public List<Affectation> listerAffectation() {
@@ -123,31 +85,31 @@ public class RepositoryAffectationImpl implements IRepositoryAffectation {
     public List<Affectation> rechercheAffectationAvecFiltres(String uid, String nom, String codeUo, String nomUsageUo, String nomSite, String numeroLigneCollaborateur, String nomModeleIphone, LocalDate dateRenouvMin, LocalDate dateRenouvMax) {
         StringBuilder query = new StringBuilder();
         query.append("select a from AffectationEntity a where 1=1 ");
-//        String query = "select a from AffectationEntity a where 1=1 ";
-        if (uid != null & uid !=""){
+
+        if (uid != null && !uid.isEmpty()){
             query.append(String.format("AND a.collaborateur.uid = '%s' ", uid ));
 
         }
-        if (nom != null & nom !=""){
+        if (nom != null && !nom.isEmpty()){
             query.append(String.format("AND a.collaborateur.nom = '%s' ", nom ));
         }
-        if (codeUo != null & codeUo !=""){
+        if (codeUo != null && !codeUo.isEmpty()){
             query.append(String.format("AND a.collaborateur.uo.codeUo = '%s' ",  codeUo));
 
         }
-        if (nomUsageUo != null & nomUsageUo !=""){
+        if (nomUsageUo != null && !nomUsageUo.isEmpty()){
             query.append(String.format("AND a.collaborateur.uo.nomUsageUo = '%s' ", nomUsageUo));
 
         }
-        if (nomSite != null & nomSite !=""){
+        if (nomSite != null && !nomSite.isEmpty()){
             query.append(String.format("AND a.collaborateur.uo.siteExercice.nomSite = '%s' ", nomSite ));
 
         }
-        if (numeroLigneCollaborateur != null & numeroLigneCollaborateur !=""){
+        if (numeroLigneCollaborateur != null && !numeroLigneCollaborateur.isEmpty()){
             query.append(String.format("AND a.collaborateur.numeroLigne = '%s' ", numeroLigneCollaborateur));
 
         }
-        if (nomModeleIphone != null & nomModeleIphone !=""){
+        if (nomModeleIphone != null && !nomModeleIphone.isEmpty()){
             query.append(String.format("AND a.iphone.modeleIphoneEntity.nomModele = '%s' ", nomModeleIphone));
         }
         if (dateRenouvMin != null){
@@ -160,7 +122,6 @@ public class RepositoryAffectationImpl implements IRepositoryAffectation {
 
         String maRequeteConstruite = query.toString();
         monLogger.debug(maRequeteConstruite);
-        System.out.println(maRequeteConstruite);
 
         List<AffectationEntity> maListEntity = monEntityManager.createQuery(maRequeteConstruite).getResultList();
         List<Affectation> maList = new ArrayList<>();
