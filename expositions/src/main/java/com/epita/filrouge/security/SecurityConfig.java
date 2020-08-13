6a8d873ab09383d2f0273f8c3eb006835ed574c3
Leopold KAMDEM DJOKO <kamdemleo@yahoo.fr>
2020-08-13 15:34:08 +0200

    Securisation suppression utilisateur, sur les 2 critères (username + password de l'utilisateur en session)

--- expositions/src/main/java/com/epita/filrouge/security/SecurityConfig.java
+++ expositions/src/main/java/com/epita/filrouge/security/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailServiceImpl).passwordEncoder(passwordEncoderBis());
+        auth.eraseCredentials(false)
+                .userDetailsService(userDetailServiceImpl).passwordEncoder(passwordEncoderBis());
     }
 
     @Override
--- expositions/src/main/resources/data.sql
+++ expositions/src/main/resources/data.sql
@@ -158,6 +158,14 @@ INSERT INTO collaborateur_entity(
 
    VALUES ('DUBOIS', '0303030303', 'Marie', '208656', 1);
 
+   INSERT INTO collaborateur_entity(
+   nom, numero_ligne, prenom, uid, uo_id)
+   VALUES ('DUPOND', '0404040404', 'François', '123456', 1);
+
+   INSERT INTO collaborateur_entity(
+   nom, numero_ligne, prenom, uid, uo_id)
+   VALUES ('LAGAFF', '0505050505', 'Gaston', '300400', 1);
+
 --Table Utilisateur
 
 INSERT INTO utilisateur_entity(
