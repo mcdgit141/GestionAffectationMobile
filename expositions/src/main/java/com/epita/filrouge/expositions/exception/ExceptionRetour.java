550bb51db5bd4da5842d7fae6674789c124efcd8
damien vivier <vivdams@gmail.com>
2020-08-21 17:34:19 +0200

    Validation des attributs de AffectationDTO : - ajout de messages pour les validation KO (@Valid + handleMethodArgumentNotValid) - suppression de certaines règles de validations - ajout de tests

--- expositions/src/main/java/com/epita/filrouge/expositions/exception/ExceptionRetour.java
+++ expositions/src/main/java/com/epita/filrouge/expositions/exception/ExceptionRetour.java
@@ -4,17 +4,23 @@ import com.epita.filrouge.domain.exception.BadRequestException;
 import com.epita.filrouge.domain.exception.BusinessException;
 import com.fasterxml.jackson.databind.exc.ValueInstantiationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.validation.ConstraintViolationException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ExceptionRetour extends ResponseEntityExceptionHandler {
@@ -41,6 +47,27 @@ public class ExceptionRetour extends ResponseEntityExceptionHandler {
         body.put(MESSAGE, message.substring(message.indexOf("problem:") + 9, message.indexOf('\n')));
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
+
+    @Override
+        protected ResponseEntity<Object> handleMethodArgumentNotValid(
+                MethodArgumentNotValidException ex, HttpHeaders headers,
+                HttpStatus status, WebRequest request) {
+
+            Map<String, Object> body = new LinkedHashMap<>();
+            body.put(TIMESTAMP, LocalDate.now());
+            body.put(STATUS, status.value());
+
+            List<String> errors = ex.getBindingResult()
+                    .getFieldErrors()
+                    .stream()
+                    .map(x -> x.getDefaultMessage())
+                    .collect(Collectors.toList());
+
+            body.put(ERROR, errors);
+
+            return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        }
+
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<Object> notFoundExceptionHandler(BusinessException businessException, WebRequest request) {
